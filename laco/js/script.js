100ace1a2c2ab9d03bc8c9fd90ee2e6cecd1f526
Sheila Barcellos <sheila.rbarcellos@senacsp.edu.br>
2024-09-25 09:59:33 -0300

    Add files via upload

---
+++ laco/js/script.js
@@ -0,0 +1,27 @@
+// while (enquanto)
+let x = 1;
+
+while(x <= 10){
+console.log(x);
+x = x + 1;
+}
+
+let y = 15;
+while(y >= 5){
+    console.log(y);
+    y--;
+}
+
+// Do While
+
+let m = 15;
+
+do{
+    console.log(`${m}`);
+    m = m + 2;
+} while (m <= 21);
+
+//for
+for(i = 0; i <= 5; i++){
+    console.log(i);
+}
