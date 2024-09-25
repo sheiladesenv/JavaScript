29eaaf362cc641bf4d0f0470ac7160112de6db27
Sheila Barcellos <sheila.rbarcellos@senacsp.edu.br>
2024-09-25 10:00:02 -0300

    Add files via upload

---
+++ operacoesmatematicas/js/script.js
@@ -0,0 +1,29 @@
+//Soma
+console.log(5+3);
+const a = 3;
+const b = 2;
+console.log(a+b);
+
+//Subtração
+console.log(3-2);
+const c = 8;
+const d = 3;
+const e = c-d;
+console.log(e);
+
+//Multiplicão
+console.log(4*2);
+const f = 2;
+const g = f*2;
+console.log(g);
+
+//Divisão
+
+console.log(5/2);
+
+//Módulo - resto da divisão
+console.log(5%2);
+const h = 10;
+const i = 5;
+console.log(h/i);
+console.log(h%i);
